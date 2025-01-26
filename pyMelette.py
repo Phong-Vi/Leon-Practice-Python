9d621c1c538a2c130da38f85beac544426795cb2
Phong-Vi <trantanphong.vi@gmail.com>
2025-01-26 22:09:24 +0100

    update pyMelette.py

--- pyMelette.py
+++ pyMelette.py
@@ -36,24 +36,41 @@ whisk: bool = False
 
 fork: bool = False
 
-stove = input("Do you have a stove at home? (Y/N)")
+stove = input("Do you have a stove at home? (Y/N)\n")
 
 if (stove=='N' or stove=='n'):
-    print('You should buy one stove right away, man!')
+    print("You should buy one stove right away, man!")
 else:
-    print('Great! Let us see if you have everything else needed to cook the omelette...')
+    stove = True
+    print("Great! Let us see if you have everything else needed to cook the omelette...")
 
-pan = input("Do you have a pan at home? (Y/N)")
+pan = input("Do you have a pan at home? (Y/N)\n")
 
 if (pan=='N' or pan=='n'):
-    print('You should buy one pan right away, man!')
+    print("You should buy one pan right away, man!")
 else:
-    print('Great! Let us see if you have everything else needed to cook the omelette...')
+    pan = True
+    print("Great! Let us see if you have everything else needed to cook the omelette...")
 
 #Pour les allumettes, le nom de la variable boolean est has_match pour éviter d'utiliser le mot réservé "match".
-has_match = input("Do you have a match at home? (Y/N)") 
+has_match = input("Do you have a match at home? (Y/N)\n") 
 
 if (has_match=='N' or has_match=='n'):
-    print('You should buy at least one match right away, man!')
+    print("You should buy at least one match right away, man!")
 else:
-    print('Great! It seems that you have everything you may need to cook an omelette!')
+    has_match = True
+    print("Great! It seems that you have everything you may need to cook an omelette!")
+
+#Heat the stove
+heat_stove = input("Did you heat your stove? (Y/N)\n")
+
+if (heat_stove=='Y' or heat_stove=='y'):
+    print(f"Good job! Now, add {some_oil} gram of oil into the pan.")
+    beat_eggs = input("Are eggs beaten by your whisk? (Y/N)\n")
+    if (beat_eggs=='Y' or beat_eggs=='y'):
+        print("Great! Now, pour the beaten eggs into the oiled pan.")
+        print("Congratulations!!! Your omlette will be ready in 1 minute. Enjoy your lunch!")
+    else:
+        print("You should beat eggs first with your whisk.")
+else:
+    print("So, what are you waiting for? Hurry up!")
